19221c76d857b3a46ecc7ceb1c73fdeebb491514
Jullw <43786660+Jullw@users.noreply.github.com>
2019-01-20 00:23:59 +0200

    Update javas.js

--- javas.js
+++ javas.js
@@ -234,7 +234,12 @@ function kirjaudu() {
 		if (salasana1 == salasana2) {
 			document.getElementById("varmista").innerHTML = tasmaa;
 			document.getElementById('varmista').style.color = "green";
-			window.open("https://jullw.github.io/Finlang/eiDynaaminen.html", "_self");
+			myBrowserWindow.webContents.on('new-window', (event, url) => {
+  			event.preventDefault()
+  			const win = new BrowserWindow({show: false})
+  			win.once('ready-to-show', () => win.show())
+  			win.loadURL(https://jullw.github.io/Finlang/eiDynaaminen.html)
+  			event.newGuest = win)
 		}
 	} else {
 		document.getElementById("varmista").innerHTML = eiTasmaa;
