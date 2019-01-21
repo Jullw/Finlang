b27fb9d80802c65364bbd664315ec5af72ba0fbe
Jullw <43786660+Jullw@users.noreply.github.com>
2019-01-21 11:47:59 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -234,14 +234,7 @@ function kirjaudu() {
 		if (salasana1 == salasana2) {
 			document.getElementById("varmista").innerHTML = tasmaa;
 			document.getElementById('varmista').style.color = "green";
-			myBrowserWindow.webContents.on('new-window', (event, url) => {
-  			event.preventDefault()
-  			const win = new BrowserWindow({show: false})
-  			win.once('ready-to-show', () => win.show())
-  			win.loadURL(https://jullw.github.io/Finlang/eiDynaaminen.html)
-  			event.newGuest = win
-		
-	}) else {
+	} else {
 		document.getElementById("varmista").innerHTML = eiTasmaa;
 		document.getElementById("selvennys").innerHTML = selvennys;
 	}
@@ -272,15 +265,8 @@ function kirjaudu2() {
 
 	if (salasanaSala.length >= 8) {
 			document.getElementById("salasanaVarmista").innerHTML = salasanaOk;
-			document.getElementById('salasanaVarmista').style.color = "green";
-		
-			myBrowserWindow.webContents.on('new-window', (event, url) => {
-  			event.preventDefault()
-  			const win = new BrowserWindow({show: false})
-  			win.once('ready-to-show', () => win.show())
-  			win.loadURL(https://jullw.github.io/Finlang/eiDynaaminen.html)
-  			event.newGuest = win
-	}) else {
+			document.getElementById('salasanaVarmista').style.color = "green";	
+	} else {
 		document.getElementById("selvennys").innerHTML = selvennys;
 	}
 }
